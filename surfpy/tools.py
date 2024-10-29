44305dbb74d2a683502d360194c465ae299bc9bc
Jamie Kent <jamie.kent510@gmail.com>
2024-10-29 08:42:45 -0700

    Use allowed_methods instead of list

--- surfpy/tools.py
+++ surfpy/tools.py
@@ -144,16 +144,16 @@ def steepness(significant_wave_height, dominant_period):
 def peakdetect(v, delta, x = None):
     """
     Converted from MATLAB script at http://billauer.co.il/peakdet.html
-    
+
     Returns two arrays
-    
+
     function [maxtab, mintab]=peakdet(v, delta, x)
     %PEAKDET Detect peaks in a vector
     %        [MAXTAB, MINTAB] = PEAKDET(V, DELTA) finds the local
     %        maxima and minima ("peaks") in the vector V.
     %        MAXTAB and MINTAB consists of two columns. Column 1
     %        contains indices in V, and column 2 the found values.
-    %      
+    %
     %        With [MAXTAB, MINTAB] = PEAKDET(V, DELTA, X) the indices
     %        in MAXTAB and MINTAB are replaced with the corresponding
     %        X-values.
@@ -161,24 +161,24 @@ def peakdetect(v, delta, x = None):
     %        A point is considered a maximum peak if it has the maximal
     %        value, and was preceded (to the left) by a value lower by
     %        DELTA.
-    
+
     % Eli Billauer, 3.4.05 (Explicitly not copyrighted).
     % This function is released to the public domain; Any use is allowed.
-    
+
     """
     min_indexes = []
     min_values = []
     max_indexes = []
     max_values = []
-       
+
     if x is None:
         x = range(len(v))
-    
+
     mn, mx = float('inf'), -float('inf')
     mnpos, mxpos = float('nan'), float('nan')
-    
+
     lookformax = True
-    
+
     for i in range(len(v)):
         this = v[i]
         if this > mx:
@@ -187,7 +187,7 @@ def peakdetect(v, delta, x = None):
         if this < mn:
             mn = this
             mnpos = x[i]
-        
+
         if lookformax:
             if this < mx-delta:
                 max_indexes.append(mxpos)
@@ -254,7 +254,7 @@ def retry_session(retries=1):
     retries = Retry(total=retries,
                 backoff_factor=0.1,
                 status_forcelist=[500, 502, 503, 504],
-                method_whitelist=frozenset(['GET', 'POST']))
+                allowed_methods=frozenset(['GET', 'POST']))
 
     session.mount('https://', HTTPAdapter(max_retries=retries))
     session.mount('http://', HTTPAdapter(max_retries=retries))
